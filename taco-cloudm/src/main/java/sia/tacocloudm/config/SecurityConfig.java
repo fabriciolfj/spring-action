50892c7a104b5e731e9b533fbd6be37fce44b92f
fabricio <fabricio.jacob@outlook.com>
2021-05-27 16:19:55 -0300

    add

--- taco-cloudm/src/main/java/sia/tacocloudm/config/SecurityConfig.java
+++ taco-cloudm/src/main/java/sia/tacocloudm/config/SecurityConfig.java
@@ -4,22 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import sia.tacocloudm.repository.UserRepository;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig  {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -41,10 +34,12 @@ public class SecurityConfig {
 
     /*
     * A ordem possui procedência, ou seja, a primeira e sobre sai sobre a segunda.
+    * usado para aplicacao mvn, para rest usa-se WebSecurityConfigurerAdapter
     * */
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.csrf().disable().authorizeRequests()
+        return http.csrf().disable()
+                .authorizeRequests()
                 .antMatchers("/design", "/orders", "/orders/current")
                 .hasRole("USER")
                 .antMatchers("/", "/**").permitAll()
